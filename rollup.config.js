58856d49377b2d63e377419d37357547a6ad5902
Jason Carini <carinij@gmail.com>
2021-03-21 14:52:18 -0700

    Added server / live server. Added configuration files to the repo.

---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+// import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import del from 'rollup-plugin-delete'
+import json from '@rollup/plugin-json';
+import html from '@rollup/plugin-html';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import nodePolyFills from 'rollup-plugin-node-polyfills';
+import { terser } from 'rollup-plugin-terser';
+
+
+
+export default {
+  input: 'src/main.ts',
+  output: [
+    {
+      file: 'dist/bundle.js',
+      format: 'iife',
+      name: 'spacetraders',
+    },
+    {
+      file: 'dist/bundle.min.js',
+      format: 'iife',
+      name: 'spacetraders',
+      plugins: [ terser() ]
+    }
+  ],
+  plugins: [
+    typescript(),
+    del({ targets: 'public/*' }),
+    json(),
+    html({ title: 'Space Traders' }),
+    nodeResolve({
+      extensions: ["js"],
+    }),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify( 'development' )
+    }),
+    // babel({
+    //   presets: ["@babel/preset-react"]
+    // }),
+    commonjs(),
+    nodePolyFills(),
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ["", "dist"],
+      host: "localhost",
+      port: 3000,
+    }),
+    livereload({
+      watch: "dist"
+    })
+  ]
+};
---
+++ src/overlay/titlescreen.ts
@@ -0,0 +1,25 @@
+<pre style="white-space: pre; color: green; background-color: black; font-size: 8px">
+                                                                                                                                                                                  dddddddd
+      SSSSSSSSSSSSSSS                                                                                    TTTTTTTTTTTTTTTTTTTTTTT                                                  d::::::d
+    SS:::::::::::::::S                                                                                   T:::::::::::::::::::::T                                                  d::::::d
+   S:::::SSSSSS::::::S                                                                                   T:::::::::::::::::::::T                                                  d::::::d
+   S:::::S     SSSSSSS                                                                                   T:::::TT:::::::TT:::::T                                                  d:::::d
+   S:::::S            ppppp   ppppppppp     aaaaaaaaaaaaa       cccccccccccccccc    eeeeeeeeeeee         TTTTTT  T:::::T  TTTTTTrrrrr   rrrrrrrrr     aaaaaaaaaaaaa       ddddddddd:::::d     eeeeeeeeeeee    rrrrr   rrrrrrrrr       ssssssssss
+   S:::::S            p::::ppp:::::::::p    a::::::::::::a    cc:::::::::::::::c  ee::::::::::::ee               T:::::T        r::::rrr:::::::::r    a::::::::::::a    dd::::::::::::::d   ee::::::::::::ee  r::::rrr:::::::::r    ss::::::::::s
+    S::::SSSS         p:::::::::::::::::p   aaaaaaaaa:::::a  c:::::::::::::::::c e::::::eeeee:::::ee             T:::::T        r:::::::::::::::::r   aaaaaaaaa:::::a  d::::::::::::::::d  e::::::eeeee:::::eer:::::::::::::::::r ss:::::::::::::s
+     SS::::::SSSSS    pp::::::ppppp::::::p           a::::a c:::::::cccccc:::::ce::::::e     e:::::e             T:::::T        rr::::::rrrrr::::::r           a::::a d:::::::ddddd:::::d e::::::e     e:::::err::::::rrrrr::::::rs::::::ssss:::::s
+       SSS::::::::SS   p:::::p     p:::::p    aaaaaaa:::::a c::::::c     ccccccce:::::::eeeee::::::e             T:::::T         r:::::r     r:::::r    aaaaaaa:::::a d::::::d    d:::::d e:::::::eeeee::::::e r:::::r     r:::::r s:::::s  ssssss
+          SSSSSS::::S  p:::::p     p:::::p  aa::::::::::::a c:::::c             e:::::::::::::::::e              T:::::T         r:::::r     rrrrrrr  aa::::::::::::a d:::::d     d:::::d e:::::::::::::::::e  r:::::r     rrrrrrr   s::::::s
+               S:::::S p:::::p     p:::::p a::::aaaa::::::a c:::::c             e::::::eeeeeeeeeee               T:::::T         r:::::r             a::::aaaa::::::a d:::::d     d:::::d e::::::eeeeeeeeeee   r:::::r                  s::::::s
+               S:::::S p:::::p    p::::::pa::::a    a:::::a c::::::c     ccccccce:::::::e                        T:::::T         r:::::r            a::::a    a:::::a d:::::d     d:::::d e:::::::e            r:::::r            ssssss   s:::::s
+   SSSSSSS     S:::::S p:::::ppppp:::::::pa::::a    a:::::a c:::::::cccccc:::::ce::::::::e                     TT:::::::TT       r:::::r            a::::a    a:::::a d::::::ddddd::::::dde::::::::e           r:::::r            s:::::ssss::::::s
+   S::::::SSSSSS:::::S p::::::::::::::::p a:::::aaaa::::::a  c:::::::::::::::::c e::::::::eeeeeeee             T:::::::::T       r:::::r            a:::::aaaa::::::a  d:::::::::::::::::d e::::::::eeeeeeee   r:::::r            s::::::::::::::s
+   S:::::::::::::::SS  p::::::::::::::pp   a::::::::::aa:::a  cc:::::::::::::::c  ee:::::::::::::e             T:::::::::T       r:::::r             a::::::::::aa:::a  d:::::::::ddd::::d  ee:::::::::::::e   r:::::r             s:::::::::::ss
+    SSSSSSSSSSSSSSS    p::::::pppppppp      aaaaaaaaaa  aaaa    cccccccccccccccc    eeeeeeeeeeeeee             TTTTTTTTTTT       rrrrrrr              aaaaaaaaaa  aaaa   ddddddddd   ddddd    eeeeeeeeeeeeee   rrrrrrr              sssssssssss
+                       p:::::p
+                       p:::::p
+                      p:::::::p
+                      p:::::::p
+                      p:::::::p
+                      ppppppppp
+    </pre>
